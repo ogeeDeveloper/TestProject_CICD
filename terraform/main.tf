5a79f5c25b74697b6e9e3c9c172ece1ff1925fbd
ogeeDeveloper <omarogrant202@hotmail.com>
2024-06-05 14:19:24 -0500

    Updated the main.tf tpo create deployment droplet if it doesn't already exists

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,12 +11,8 @@ provider "digitalocean" {
   token = var.do_token
 }
 
-data "digitalocean_droplet" "existing_droplet" {
-  name = "app-server"
-}
-
 resource "digitalocean_droplet" "app_server" {
-  count  = length(data.digitalocean_droplet.existing_droplet.id) == 0 ? 1 : 0
+  count  = length(data.digitalocean_droplet.existing_droplet) == 0 ? 1 : 0
   image  = "ubuntu-20-04-x64"
   name   = "app-server"
   region = "nyc3"
@@ -24,9 +20,15 @@ resource "digitalocean_droplet" "app_server" {
   ssh_keys = [var.ssh_key_id]  # Ensure your SSH key is added to DigitalOcean
 }
 
+data "digitalocean_droplet" "existing_droplet" {
+  count = 0
+  name = "app-server"
+  depends_on = [digitalocean_droplet.app_server]
+}
+
 output "app_server_ip" {
   value = coalesce(
-    data.digitalocean_droplet.existing_droplet.ipv4_address,
+    data.digitalocean_droplet.existing_droplet[0].ipv4_address,
     digitalocean_droplet.app_server[0].ipv4_address
   )
 }

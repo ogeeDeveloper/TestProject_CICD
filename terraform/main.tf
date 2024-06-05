a4ef285d6fc9ead526b1f85f0a10bd458ed38fd4
ogeeDeveloper <omarogrant202@hotmail.com>
2024-06-05 16:11:26 -0500

    handle the creation of the droplet and removed the reading if the dropplete is not yet created

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,42 +11,25 @@ provider "digitalocean" {
   token = var.do_token
 }
 
-# Check if the droplet exists
-data "digitalocean_droplet" "existing_droplet" {
-  name   = "app-server"
-  count  = 1
-  depends_on = [time_sleep.wait_for_droplet]
+locals {
+  droplet_exists = length(data.digitalocean_droplet.existing_droplet) > 0
 }
 
-resource "time_sleep" "wait_for_droplet" {
-  depends_on = []
-  create_duration = "10s"
+data "digitalocean_droplet" "existing_droplet" {
+  name = "app-server"
+  count = 0
 }
 
 resource "digitalocean_droplet" "app_server" {
-  count     = length(data.digitalocean_droplet.existing_droplet) == 0 ? 1 : 0
-  image     = "ubuntu-20-04-x64"
-  name      = "app-server"
-  region    = "nyc3"
-  size      = "s-1vcpu-1gb"
+  count = local.droplet_exists ? 0 : 1
+  image = "ubuntu-20-04-x64"
+  name = "app-server"
+  region = "nyc3"
+  size = "s-1vcpu-1gb"
   monitoring = true
-  ssh_keys  = [var.ssh_key_id]
-}
-
-locals {
-  existing_droplet_count = length(data.digitalocean_droplet.existing_droplet)
-  existing_droplet_ids   = data.digitalocean_droplet.existing_droplet.*.id
-  existing_droplet_ip    = length(data.digitalocean_droplet.existing_droplet) > 0 ? data.digitalocean_droplet.existing_droplet[0].ipv4_address : ""
-}
-
-output "existing_droplet_ids" {
-  value = local.existing_droplet_ids
-}
-
-output "existing_droplet_ip" {
-  value = local.existing_droplet_ip
+  ssh_keys = [var.ssh_key_id]
 }
 
 output "app_server_ip" {
-  value = local.existing_droplet_count > 0 ? local.existing_droplet_ip : digitalocean_droplet.app_server[0].ipv4_address
+  value = local.droplet_exists ? data.digitalocean_droplet.existing_droplet[0].ipv4_address : digitalocean_droplet.app_server[0].ipv4_address
 }

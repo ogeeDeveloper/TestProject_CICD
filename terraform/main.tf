83d1f07667759b4225b13fa0c9d77d420f4f45d7
ogeeDeveloper <omarogrant202@hotmail.com>
2024-06-05 16:19:58 -0500

    updated the main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,25 +11,27 @@ provider "digitalocean" {
   token = var.do_token
 }
 
-locals {
-  droplet_exists = length(data.digitalocean_droplet.existing_droplet) > 0
-}
-
 data "digitalocean_droplet" "existing_droplet" {
-  name = "app-server"
-  count = 0
+  name   = "app-server"
+  count  = 0
 }
 
 resource "digitalocean_droplet" "app_server" {
-  count = local.droplet_exists ? 0 : 1
-  image = "ubuntu-20-04-x64"
-  name = "app-server"
-  region = "nyc3"
-  size = "s-1vcpu-1gb"
+  count     = length(data.digitalocean_droplet.existing_droplet) == 0 ? 1 : 0
+  image     = "ubuntu-20-04-x64"
+  name      = "app-server"
+  region    = "nyc3"
+  size      = "s-1vcpu-1gb"
   monitoring = true
-  ssh_keys = [var.ssh_key_id]
+  ssh_keys  = [var.ssh_key_id]
+}
+
+locals {
+  existing_droplet_count = length(data.digitalocean_droplet.existing_droplet)
+  existing_droplet_ids   = data.digitalocean_droplet.existing_droplet.*.id
+  existing_droplet_ip    = length(data.digitalocean_droplet.existing_droplet) > 0 ? data.digitalocean_droplet.existing_droplet[0].ipv4_address : ""
 }
 
 output "app_server_ip" {
-  value = local.droplet_exists ? data.digitalocean_droplet.existing_droplet[0].ipv4_address : digitalocean_droplet.app_server[0].ipv4_address
+  value = local.existing_droplet_count > 0 ? local.existing_droplet_ip : digitalocean_droplet.app_server[0].ipv4_address
 }

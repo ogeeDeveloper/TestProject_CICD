9e5c316856ef4745e3497800decc9026f956af91
ogeeDeveloper <omarogrant202@hotmail.com>
2024-06-05 14:42:13 -0500

    updated main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,16 +11,10 @@ provider "digitalocean" {
   token = var.do_token
 }
 
-variable "do_token" {}
-variable "ssh_key_id" {}
-
-# Try to look up an existing droplet
 data "digitalocean_droplet" "existing_droplet" {
   name = "app-server"
-  depends_on = []
 }
 
-# If the droplet doesn't exist, create it
 resource "digitalocean_droplet" "app_server" {
   count  = length(data.digitalocean_droplet.existing_droplet.*.id) == 0 ? 1 : 0
   image  = "ubuntu-20-04-x64"
@@ -30,7 +24,6 @@ resource "digitalocean_droplet" "app_server" {
   ssh_keys = [var.ssh_key_id]  # Ensure your SSH key is added to DigitalOcean
 }
 
-# Output the IP address of the droplet, whether it was created or already existed
 output "app_server_ip" {
   value = length(data.digitalocean_droplet.existing_droplet.*.id) > 0 ? data.digitalocean_droplet.existing_droplet[0].ipv4_address : digitalocean_droplet.app_server[0].ipv4_address
 }
